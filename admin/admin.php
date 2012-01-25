9a471a9ebf76c5ec1d13ba8ca6b8355537a55877
Ignazios <Ignazios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-25 17:49:03 +0000

    git-svn-id: https://plugins.svn.wordpress.org/albo-pretorio-on-line/tags/1.9@495284 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/admin.php
@@ -0,0 +1,270 @@
+<?php
+/**
+ * Albo Pretorio AdminPanel - Amministrazione Albo Pretorio
+ * 
+ * @package Albo Pretorio On line
+ * @author Scimone Ignazio
+ * @copyright 2011-2014
+ * @since 1.9
+ */
+
+require_once(ABSPATH . 'wp-includes/pluggable.php'); 
+
+switch ( $_REQUEST['action'] ) {
+		case 'updatecss':
+			$location = "?page=editorcss";
+			$file=stripslashes( $_REQUEST['file']);
+			$newcontent = stripslashes($_REQUEST['newcontent']);
+			if (is_writeable($file)) {
+				//is_writable() not always reliable, check return value. see comments @ http://uk.php.net/is_writable
+				
+				$f = fopen($file, 'w+');
+				if ($f !== FALSE) {
+					fwrite($f, $newcontent);
+					fclose($f);
+					$location.="&a=1";
+				} 
+			}
+			wp_redirect( $location );
+		break;	
+		case "delete-allegato-atto" :
+		$location = "?page=categorie" ;
+		ap_del_allegato_atto($_REQUEST['idAllegato'],$_REQUEST['idAtto'],$_REQUEST['Allegato']);
+		$_SERVER['REQUEST_URI'] = remove_query_arg(array('message'), $_SERVER['REQUEST_URI']);
+		$_SERVER['REQUEST_URI'] = remove_query_arg(array('action'), $_SERVER['REQUEST_URI']);
+		$_SERVER['REQUEST_URI'] = remove_query_arg(array('idAllegato'), $_SERVER['REQUEST_URI']);
+		$_SERVER['REQUEST_URI'] = remove_query_arg(array('Allegato'), $_SERVER['REQUEST_URI']);
+		$location= add_query_arg( array ( 'action' => 'allegati-atto', 'id' => $_REQUEST['idAtto'] ));
+		wp_redirect( $location );
+		break;
+	case 'add-responsabile':
+		$location = "?page=responsabili" ;
+		if (!is_email( $_REQUEST['resp-email'] )){
+			$location = add_query_arg( 'errore', 'Email non valida', $location );
+			$location = add_query_arg( 'message', 4, $location );
+			$location = add_query_arg( 'resp-cognome', $_POST['resp-cognome'], $location );
+			$location = add_query_arg( 'resp-nome', $_POST['resp-nome'], $location );
+			$location = add_query_arg( 'resp-email', $_POST['resp-email'], $location );
+			$location = add_query_arg( 'resp-telefono', $_POST['resp-telefono'], $location );
+			$location = add_query_arg( 'resp-orario', $_POST['resp-orario'], $location );
+			$location = add_query_arg( 'resp-note', $_POST['resp-note'], $location );
+			$location = add_query_arg( 'action', 'add', $location );
+		}
+		else{
+			$ret=ap_insert_responsabile($_POST['resp-cognome'],$_POST['resp-nome'],$_POST['resp-email'],$_POST['resp-telefono'],$_POST['resp-orario'],$_POST['resp-note']);
+			if ( !$ret && !is_wp_error( $ret ) )
+				$location = add_query_arg( 'message', 1, $location );
+			else
+				$location = add_query_arg( 'message', 4, $location );
+		}
+		wp_redirect( $location );
+		break;
+	case 'edit-responsabile':
+		$location = "?page=responsabili" ;
+		$location = add_query_arg( 'id', $_GET['id'], $location );
+		$location = add_query_arg( 'action', 'edit', $location );
+		wp_redirect( $location );
+		break;
+	case 'memo-responsabile':
+		$location = "?page=responsabili" ;
+		if (!is_email( $_REQUEST['resp-email'] )){
+			$location = add_query_arg( 'errore', 'Email non valida', $location );
+			$location = add_query_arg( 'message', 5, $location );
+			$location = add_query_arg( 'resp-cognome', $_REQUEST['resp-cognome'], $location );
+			$location = add_query_arg( 'resp-nome', $_REQUEST['resp-nome'], $location );
+			$location = add_query_arg( 'resp-email', $_REQUEST['resp-email'], $location );
+			$location = add_query_arg( 'resp-telefono', $_REQUEST['resp-telefono'], $location );
+			$location = add_query_arg( 'resp-orario', $_REQUEST['resp-orario'], $location );
+			$location = add_query_arg( 'resp-note', $_REQUEST['resp-note'], $location );
+			$location = add_query_arg( 'action', 'edit_err', $location );
+			$location = add_query_arg( 'id', $_REQUEST['id'], $location );
+		}
+		else
+			if (ap_memo_responsabile($_REQUEST['id'],
+								  $_REQUEST['resp-cognome'],
+								  $_REQUEST['resp-nome'],
+								  $_REQUEST['resp-email'],
+								  $_REQUEST['resp-telefono'],
+								  $_REQUEST['resp-orario'],
+								  $_REQUEST['resp-note']))
+				$location = add_query_arg( 'message', 3, $location );
+			else
+				$location = add_query_arg( 'message', 5, $location );
+//		global $wpdb;
+//		echo $wpdb->last_query;exit; 
+		wp_redirect( $location );
+		break;
+	case 'delete-responsabile':
+		$location = "?page=responsabili" ;
+		$res=ap_del_responsabile($_GET['id']);
+		if (!is_array($res))
+			$location = add_query_arg( 'message', 2, $location );
+		else{
+			if ($res['atti']>0)
+				$location = add_query_arg( 'message', 7, $location );
+			else
+				$location = add_query_arg( 'message', 6, $location );
+		}
+		wp_redirect( $location );
+		break;
+	case 'add-categorie':
+		$location = "?page=categorie" ;
+		$ret=ap_insert_categoria($_POST['cat-name'],$_POST['cat-parente'],$_POST['cat-descrizione'],$_POST['cat-durata']);
+		if ( !$ret && !is_wp_error( $ret ) )
+			$location = add_query_arg( 'message', 1, $location );
+		else
+			$location = add_query_arg( 'message', 4, $location );
+		wp_redirect( $location );
+		break;
+	case 'delete-categorie':
+		$location = "?page=categorie" ;
+		$res=ap_del_categorie($_GET['id']);
+		if (!is_array($res))
+			$location = add_query_arg( 'message', 2, $location );
+		else{
+			if ($res['atti']>0) {
+				$location = add_query_arg( 'message', 8, $location );
+			}else{
+				if ($res['figli']>0) {
+					$location = add_query_arg( 'message', 7, $location );
+				}
+			}
+		}
+		wp_redirect( $location );
+		break;
+	case 'edit-categorie':
+		$location = "?page=categorie" ;
+		$location = add_query_arg( 'id', $_GET['id'], $location );
+		$location = add_query_arg( 'action', 'edit', $location );
+		wp_redirect( $location );
+		break;
+	case 'memo-categoria':
+		$location = "?page=categorie" ;
+		if (!is_wp_error( ap_memo_categorie($_REQUEST['id'],
+							  $_REQUEST['cat-name'],
+							  $_REQUEST['cat-parente'],
+							  $_REQUEST['cat-descrizione'],
+							  $_REQUEST['cat-durata'])))
+			$location = add_query_arg( 'message', 3, $location );
+		else
+			$location = add_query_arg( 'message', 5, $location );
+			
+//		global $wpdb;
+//		echo $wpdb->last_query;exit; 
+		wp_redirect( $location );
+		break;
+ 	case "delete-atto":
+ 		$location = "?page=atti" ;
+		$res=ap_del_atto($_GET['id']);
+		if (!is_array($res))
+			$location = add_query_arg( 'message', 2, $location );
+		else{
+			if ($res['allegati']>0) {
+				$location = add_query_arg( 'message', 7, $location );
+			}
+		}
+		wp_redirect( $location );
+		break;
+	case "add-atto" :
+		$location = "?page=atti" ;
+		$ret=ap_insert_atto($_POST['Data'],
+		                    $_POST['Riferimento'],
+							$_POST['Oggetto'],
+							$_POST['DataInizio'],
+							$_POST['DataFine'],
+							$_POST['Note'],
+							$_POST['Categoria'],
+							$_POST['Responsabile']);
+		if ( !$ret && !is_wp_error( $ret ) )
+			$location = add_query_arg( 'message', 1, $location );
+		else
+			$location = add_query_arg( 'message', 4, $location );
+		wp_redirect( $location );
+		break;
+	case "memo-atto" :
+		$location = "?page=atti" ;
+		$ret=ap_memo_atto($_REQUEST['id'],
+		                  $_POST['Data'],
+		                  $_POST['Riferimento'],
+						  $_POST['Oggetto'],
+						  $_POST['DataInizio'],
+						  $_POST['DataFine'],
+						  $_POST['Note'],
+						  $_POST['Categoria'],
+						  $_POST['Responsabile']);
+		if ( !$ret && !is_wp_error( $ret ) )
+			$location = add_query_arg( 'message', 3, $location );
+		else
+			$location = add_query_arg( 'message', 5, $location );
+		wp_redirect( $location );
+		break;
+	case "memo-allegato-atto":
+		$location = "?page=atti" ;
+		$messaggio =addslashes(str_replace(" ","%20",Memo_allegato_atto()));
+		$location = add_query_arg(array ( 'action' => 'allegati-atto', 
+		                                  'messaggio' => $messaggio,
+										  'id' => $_REQUEST['id']) , $location );
+		wp_redirect( $location );
+		break;	
+	case "update-allegato-atto":
+		$location='?page=atti&action=allegati-atto&id='.$_REQUEST['id'];
+		if ($_REQUEST['submit']=="Annulla"){
+			wp_redirect( $location );
+		}else{
+			$ret=ap_memo_allegato($_REQUEST['idAlle'],$_REQUEST['titolo'],$_REQUEST['id']);
+			if ( is_object($ret)){
+				$location = add_query_arg( 'messaggio', str_replace(' ',"%20",$ret->get_error_message()), $location );	
+			}
+			else{
+			 	$location = add_query_arg( 'messaggio', "Allegato%20Aggiornato", $location );
+			}
+			wp_redirect( $location );		
+		}
+		break;
+}
+
+function Memo_allegato_atto(){
+	if ($_REQUEST["operazione"]=="upload"){
+	 	if ((($_FILES["file"]["size"] / 1024)/1024)<1){
+			$DimFile=number_format($_FILES["file"]["size"] / 1024,2);
+			$UnitM=" KB";
+		}else{
+			$DimFile=number_format(($_FILES["file"]["size"] / 1024)/1024,2);	
+			$UnitM=" MB";
+		}
+	    $dime= "Dimensione: " . $DimFile . " ".$UnitM;
+		if ($_FILES['file']['tmp_name']==''){
+			$messages[4]= "Fine non selezionato Oppure operazione annullata";
+		}else{
+			if ($_FILES["file"]["type"] != "application/pdf"){
+				$messages= "Tipo file non valido, sono ammessi soltanto i file in formato PDF";
+			}else{
+			 	
+				if (($DimFile>20) and ($UnitM==" MB")){
+					$messages= "Il file caricato è di ".$DimFile." Mb, il limite massimo &egrave; di 20 Mb";
+				}else{
+				  if ($_FILES["file"]["error"] > 0){
+					$messages= "Errore: " . $_FILES["file"]["error"];
+		    	}else{
+					$destination_path = addslashes(AP_BASE_DIR).get_option('opt_AP_FolderUpload').'/';
+			   		$result = 0;
+			   		UniqueFileName(stripcslashes($destination_path) . basename( $_FILES['file']['name']));
+				   	$target_path = UniqueFileName(stripcslashes($destination_path) . basename( $_FILES['file']['name']));
+					   if(@move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
+	    				$messages= "File caricato%%br%%Nome: " . basename( $target_path);
+	    				ap_insert_allegato($_REQUEST['Descrizione'],$target_path,$_REQUEST['id']);
+			   		}else{
+						$messages= "Il File non caricato: " . $target_path;
+					}
+				}
+		  	}
+		  }
+		}
+		$msg=($messages!="") ? ($messages): ""; 
+		$msg.=($dime!="") ?   "%%br%%" .($dime): "";
+		$messages=$msg;
+	}
+	return $messages;
+}
+
+?>
---
+++ admin/frontend.php
@@ -0,0 +1,327 @@
+<?php
+/**
+ * Albo Pretorio AdminPanel - Funzioni Accesso ai dati
+ * 
+ * @package Albo Pretorio On line
+ * @author Scimone Ignazio
+ * @copyright 2011-2014
+ * @since 1.9
+ */
+
+if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { die('You are not allowed to call this page directly.'); }
+
+
+switch ($_REQUEST['action']){
+	case 'visatto':
+		VisualizzaAtto($_REQUEST['id'],$_REQUEST['rif']);
+		break;
+	case 'categoria':
+		Lista_Atti($Parametri,0,$_REQUEST['cat']);
+		break;
+	case 'addstatall':
+		ap_insert_log(6,5,$_GET['id'],"Download",$_GET['idAtto']);
+		break;
+	default: 
+		switch ($_REQUEST['filtra']){
+		 	case 'Filtra':
+		 		Lista_Atti($Parametri,$_REQUEST['anno'], 0,$_REQUEST['oggetto'],$_REQUEST['DataInizio'],$_REQUEST['DataFine']);
+		 		break;
+		 	default:
+			 	Lista_Atti($Parametri);
+			 	break;
+		}
+}
+
+function VisualizzaAtto($id,$rif){
+	$risultato=ap_get_atto($id);
+	$risultato=$risultato[0];
+	$risultatocategoria=ap_get_categoria($risultato->IdCategoria);
+	$risultatocategoria=$risultatocategoria[0];
+	$allegati=ap_get_all_allegati_atto($id);
+	$responsabile=ap_get_responsabile($risultato->RespProc);
+	$responsabile=$responsabile[0];
+	ap_insert_log(5,5,$id,"Visualizzazione");
+	echo '
+<div class="Visalbo">
+<h2>Dati atto</h2>
+<a href="'.$rif.'" title="Torna alla lista degli atti">Torna alla Lista</a>
+<table class="tabVisalbo">
+	    <tbody id="dati-atto">
+		<tr>
+			<th>Numero Albo</th>
+			<td>'.$risultato->Numero."/".$risultato->Anno.'</td>
+		</tr>
+		<tr>
+			<th>Data</th>
+			<td>'.VisualizzaData($risultato->Data).'</td>
+		</tr>
+		<tr>
+			<th>Codice di Riferimento</th>
+			<td>'.stripslashes($risultato->Riferimento).'</td>
+		</tr>
+		<tr>
+			<th>Oggetto</th>
+			<td>'.stripslashes($risultato->Oggetto).'</td>
+		</tr>
+		<tr>
+			<th>Data inizio Pubblicazione</th>
+			<td>'.VisualizzaData($risultato->DataInizio).'</td>
+		</tr>
+		<tr>
+			<th>Data fine Pubblicazione</th>
+			<td>'.VisualizzaData($risultato->DataFine).'</td>
+		</tr>
+		<tr>
+			<th>Note</th>
+			<td>'.stripslashes($risultato->Informazioni).'</td>
+		</tr>
+		<tr>
+			<th>Categoria</th>
+			<td>'.stripslashes($risultatocategoria->Nome).'</td>
+		</tr>
+	    </tbody>
+	</table>';
+if ($responsabile){
+	echo '<h3>Responsabile</h3>
+	<div class="Visallegato">
+			<table class="tabVisResp">
+	    		<tbody id="dati-responsabile">
+				<tr>
+					<th>Persona</th>
+					<td>'.$responsabile->Cognome." ".$responsabile->Nome.'</td>
+				</tr>
+				<tr>
+					<th>email</th>
+					<td><a href="mailto:'.$responsabile->Email.'">'.$responsabile->Email.'</a></td>
+				</tr>
+				<tr>
+					<th>Telefono</th>
+					<td>'.$responsabile->Telefono.'</td>
+				</tr>
+				<tr>
+					<th>Orario ricevimento</th>
+					<td>'.$responsabile->Orario.'</td>
+				</tr>';
+if ($responsabile->Note)
+	echo'
+				<tr>
+					<th>Note</th>
+					<td>'.$responsabile->Note.'</td>
+				</tr>';
+echo'
+			    </tbody>
+			</table>				    
+	</div>';
+	
+}
+echo '<h3>Allegati</h3>';
+//print_r($_SERVER);
+foreach ($allegati as $allegato) {
+	echo '<div class="Visallegato">
+			<div style="float:left;display:inline;width: 60px;height:60px;">
+				    <img src="'.Albo_URL.'img/Pdf.png" alt="Icona Visualizza Atto" style="margin-left:10px;margin-top:10px;"/>
+			</div>
+			<div>
+				<p>
+					'.$allegato->TitoloAllegato.' <br />
+					<a href="'.DaPath_a_URL($allegato->Allegato).'" onclick="window.open(this.href,\'_blank\');return false;" class="addstatdw" rel="'.$_SERVER['HTTP_REFERER'].'?action=addstatall&amp;id='.$allegato->IdAllegato.'&amp;idAtto='.$id.'" >'. basename( $allegato->Allegato).'</a>
+				</p>
+			</div>
+		</div>';
+	}
+echo '
+</div>
+';	
+}
+function VisualizzaRicerca(){
+$anni=ap_get_dropdown_anni_atti('anno','anno','postform','','',1); 
+$categorie=ap_get_dropdown_categorie('categorie','categorie','postform','','',false,true); 
+Bonifica_Url();
+if (strpos($_SERVER['REQUEST_URI'],"?")>0)
+	$sep="&amp;";
+else
+	$sep="?";
+$titFiltri=get_option('opt_AP_LivelloTitoloFiltri');
+if ($titFiltri=='')
+	$titFiltri="h3";
+$HTML='<div class="ricerca">
+	<div class="ricerca_col_SX">
+		<'.$titFiltri.'>Filtri</'.$titFiltri.'>
+		<form id="filtro-atti" action="'.$_SERVER['REQUEST_URI'].'" method="get">
+		<fieldset>';
+		if (strpos($_SERVER['REQUEST_URI'],'page_id')>0){
+			$HTML.= '<input type="hidden" name="page_id" value="'.Estrai_PageID_Url().'" />';
+		}	
+$HTML.= '
+			<table id="tabella-filtro-atti" class="tabella-dati-albo" style="text-align:left;">
+				<tr>
+					<th>Anno</th>
+					<td>'.$anni.'</td>
+				</tr>
+				<tr>
+					<th>Oggetto</th>
+					<td><input type="text" size="35" maxlength="150" name="oggetto" /></td>
+				</tr>
+				<tr>
+					<th>da Data</th>
+					<td><input name="DataInizio" id="Calendario1" type="text" value="" size="8" /></td>
+				</tr>
+				<tr>
+					<th>a Data</th>
+					<td><input name="DataFine" id="Calendario2" type="text" value="" size="8" /></td>
+				</tr>
+				<tr>
+					<td colspan="2" style="text-align:center;"><input type="submit" name="filtra" id="filtra" class="bottoneFE" value="Filtra"  /></td>
+				</tr>		
+			</table>
+			</fieldset>
+		</form>
+	</div>
+	<div class="ricerca_col_DX">
+		<'.$titFiltri.'>Categorie</'.$titFiltri.'>
+		<p class="finestra_ricerca">'
+		.ap_get_nuvola_categorie($_SERVER['REQUEST_URI'].$sep."action=categoria&amp;cat",1).'
+		</p>
+	</div>
+	<br style="clear:both;" />
+</div>';	
+return $HTML;
+}
+
+function Lista_Atti($Parametri,$Anno=0,$Categoria=0,$Oggetto='',$Dadata=0,$Adata=0){
+switch ($Parametri['stato']){
+		case 0:
+			$TitoloAtti="Tutti gli Atti";
+			break;
+		case 1:
+			$TitoloAtti="Atti in corso di Validit&agrave;";
+			break;
+		case 2:
+			$TitoloAtti="Atti Scaduti";
+			break;
+		case 3:
+			$TitoloAtti="Atti da Pubblicare";
+			break;
+}
+	if (isset($Parametri['per_page'])){
+		$N_A_pp=$Parametri['per_page'];	
+	}else{
+		$N_A_pp=100;
+	}
+	if (!isset($_REQUEST['Pag'])){
+		$Da=0;
+		$A=$N_A_pp;
+	}else{
+		$Da=($_REQUEST['Pag']-1)*$N_A_pp;
+		$A=$N_A_pp;
+	}
+	$TotAtti=ap_get_all_atti($Parametri['stato'],$Anno,$Categoria,$Oggetto,$Dadata,$Adata,'',0,0,true);
+	$lista=ap_get_all_atti($Parametri['stato'],$Anno,$Categoria,$Oggetto,$Dadata,$Adata,'',$Da,$A); 
+$titEnte=get_option('opt_AP_LivelloTitoloEnte');
+if ($titEnte=='')
+	$titEnte="h2";
+$titPagina=get_option('opt_AP_LivelloTitoloPagina');
+if ($titPagina=='')
+	$titPagina="h3";
+echo' <div class="Visalbo">';
+if (get_option('opt_AP_VisualizzaEnte')=='Si')
+		echo '<'.$titEnte.' ><span  class="titoloEnte">'.stripslashes(get_option('opt_AP_Ente')).'</span></'.$titEnte.'>';
+echo'<'.$titPagina.'><span  class="titoloPagina">'.$TitoloAtti.'</span></'.$titPagina.'>
+		'.VisualizzaRicerca().'
+		<br class="clear" />';
+	if ($TotAtti>$N_A_pp){
+	    $Para='';
+	    foreach ($_REQUEST as $k => $v){
+			if ($k!="Pag")
+				if ($Para=='')
+					$Para.=$k.'='.$v;
+				else
+					$Para.='&amp;'.$k.'='.$v;
+		}
+		if ($Para=='')
+			$Para="?Pag=";
+		else
+			$Para="?".$Para."&amp;Pag=";
+		$Npag=(int)$TotAtti/$N_A_pp;
+		if ($TotAtti%$N_A_pp>0){
+			$Npag++;
+		}
+		echo '  	<div class="tablenav" style="float:right;" id="risultati">
+		<div class="tablenav-pages">
+    		<p><strong>N. Atti '.$TotAtti.'</strong>&nbsp;&nbsp; Pagine';
+    	if (isset($_REQUEST['Pag']) And $_REQUEST['Pag']>1 ){
+			$Pagcur=$_REQUEST['Pag'];
+			$PagPre=$Pagcur-1;
+			echo '&nbsp;<a href="'.$Para.$PagPre.'" class="next page-numbers">&laquo;</a>';
+		}else{
+			$Pagcur=1;
+		}
+		for($i=1;$i<=$Npag;$i++){
+			if ($i==$Pagcur){
+				echo '&nbsp;<span class="page-numbers current">'.$i.'</span>';
+			}else{
+				echo '&nbsp;<a href="'.$Para.$i.'" class="page-numbers" >'.$i.'</a>';		
+			}
+		}
+		$PagSuc=$Pagcur+1;
+	   	if ($PagSuc<=$Npag){
+			echo '&nbsp;<a href="'.$Para.$PagSuc.'" class="next page-numbers">&raquo;</a>';
+		}
+	echo'			</p>
+    	</div>
+	</div>';
+	}		
+echo '	<div class="tabalbo">
+		<table id="elenco-atti" class="tabella-dati-albo"> 
+	    <thead>
+	    	<tr>
+	        	<th>progressivo</th>
+	        	<th>Riferimento</th>
+	        	<th>Oggetto</th>
+	        	<th>Validit&agrave;</th>
+	        	<th>Categoria</th>
+			</tr>
+	    </thead>
+	    <tbody>';
+	    $riferimento=$_SERVER['REDIRECT_URL'];
+		if ($_SERVER['QUERY_STRING']) 
+			$riferimento.="?".$_SERVER['QUERY_STRING'];
+	if ($lista){
+		foreach($lista as $riga){
+		$categoria=ap_get_categoria($riga->IdCategoria);
+		$cat=$categoria[0]->Nome;
+		$NumeroAtto=ap_get_num_anno($riga->IdAtto);
+		Bonifica_Url();
+		if (strpos($_SERVER['REQUEST_URI'],"?")>0)
+			$sep="&amp;";
+		else
+			$sep="?";
+		echo '<tr>
+		        <td><a href="'.$_SERVER['REQUEST_URI'].$sep.'action=visatto&amp;id='.$riga->IdAtto.'&amp;rif='.$riferimento.'"  >'.$NumeroAtto.'/'.$riga->Anno .'</a><br />'.VisualizzaData($riga->Data).'
+				</td>
+				<td>
+					'.$riga->Riferimento .'
+				</td>
+				<td>
+					'.$riga->Oggetto .'  
+				</td>
+				<td>
+					'.VisualizzaData($riga->DataInizio) .'<br />'.VisualizzaData($riga->DataFine) .'  
+				</td>
+				<td>
+					'.$cat .'  
+				</td>
+			</tr>'; 
+		}
+	} else {
+			echo '<tr>
+					<td colspan="6">Nessun Atto Codificato</td>
+				  </tr>';
+	}
+	echo '
+     </tbody>
+    </table>';
+echo '</div>
+</div><!-- /wrap -->	';
+}
+?>
